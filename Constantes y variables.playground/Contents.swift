e5c48f8dc3eef949799eb4f01580acaa47c17f4c
José Manuel  <josemanuel@iMac-de-Jose.local>
2023-12-15 18:33:03 +0100

    playground parte 1

---
+++ Constantes y variables.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var miVariable: String = "Hola mundo"
+
+print(miVariable)
+
+miVariable = "Adios mundo"
+
+print(miVariable)
+
+let miConstante = "Hola mundo"
+
+print(miConstante)
